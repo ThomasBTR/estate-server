7a08700c7465ca366d075c489bc24d02f2cc2cc0
ThomasBTR <thomatito971@gmail.com>
2023-02-24 14:40:53 +0100

    init project

---
+++ src/main/java/com/estate/estateserver/EstateServerApplication.java
@@ -0,0 +1,13 @@
+package com.estate.estateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estate/estateserver/EstateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estate.estateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
