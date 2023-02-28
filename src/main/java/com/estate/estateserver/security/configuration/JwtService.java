1c4c9fa3a7f2231d34b31e32cb7c47febf00996d
ThomasBTR <thomatito971@gmail.com>
2023-02-28 06:44:43 +0100

    update generateToken in jwtService

--- src/main/java/com/estate/estateserver/security/configuration/JwtService.java
+++ src/main/java/com/estate/estateserver/security/configuration/JwtService.java
@@ -2,11 +2,18 @@ package com.estate.estateserver.security.configuration;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,8 +21,34 @@ public class JwtService {
 
     private static final String SECRET_KEY = "404E635266556A576E5A7234753778214125442A472D4B6150645367566B5970";
 
+    @Value(value = "${estate.jwt.expiration}")
+    private long expirationTime;
+    
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
