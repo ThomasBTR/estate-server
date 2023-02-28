efa96577b3203516519043490d683e387412fbdb
ThomasBTR <thomatito971@gmail.com>
2023-02-28 06:31:28 +0100

    update with config jwt

--- src/main/java/com/estate/estateserver/security/repositories/IUserRepository.java
+++ src/main/java/com/estate/estateserver/security/repositories/IUserRepository.java
@@ -1,10 +1,12 @@
-package com.estate.estateserver.repositories;
+package com.estate.estateserver.security.repositories;
 
-import com.estate.estateserver.models.User;
+import com.estate.estateserver.security.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
