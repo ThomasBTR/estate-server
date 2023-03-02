f89a4d3a798734e18991fc86b14894275121f12c
ThomasBTR <thomatito971@gmail.com>
2023-03-02 11:57:47 +0100

    working security measures

--- src/main/java/com/estate/estateserver/auth/AuthenticationRequest.java
+++ src/main/java/com/estate/estateserver/auth/AuthenticationRequest.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
-    private String email;
-    private String password;
+    private String login;
+    String password;
 }
