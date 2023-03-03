c4820fb4e82a6792406a7e0b549f2cdb3ba1575b
ThomasBTR <thomatito971@gmail.com>
2023-03-03 04:38:55 +0100

    update test to gather context and remove maven wrapper

--- src/test/java/com/estate/estateserver/EstateServerApplicationTests.java
+++ src/test/java/com/estate/estateserver/EstateServerApplicationTests.java
@@ -3,8 +3,10 @@ package com.estate.estateserver;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {EstateServerApplication.class})
 class EstateServerApplicationTests {
 
 
