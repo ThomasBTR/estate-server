5906d34e062d105a0519a6764568a760c219d07e
ThomasBTR <thomatito971@gmail.com>
2023-03-15 17:15:18 +0100

    refacto logging / PUT set up with body TODO use form data

--- src/main/java/com/estate/estateserver/services/AuthenticationServices.java
+++ src/main/java/com/estate/estateserver/services/AuthenticationServices.java
@@ -13,6 +13,8 @@ import com.estate.estateserver.models.responses.UserResponse;
 import com.estate.estateserver.repositories.ITokenRepository;
 import com.estate.estateserver.repositories.IUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,12 +26,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class AuthenticationService {
+public class AuthenticationServices {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationServices.class);
     private final IUserRepository userRepository;
-
     private final PasswordEncoder encoder;
-
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     private final ITokenRepository tokenRepository;
@@ -47,6 +48,7 @@ public class AuthenticationService {
         User savedUser = getSavedUser(user);
         String token = jwtService.generateToken(savedUser);
         saveUserToken(savedUser, token);
+        LOGGER.debug("User registered: {}", savedUser.getEmail());
         return AuthenticationResponse.builder()
                 .token(token)
                 .build();
@@ -63,6 +65,7 @@ public class AuthenticationService {
         String jwtToken = jwtService.generateToken(user);
         revokeAllUserTokens(user);
         saveUserToken(user, jwtToken);
+        LOGGER.debug("User logged successfully: {}", user.getEmail());
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
@@ -72,6 +75,7 @@ public class AuthenticationService {
     public UserResponse me(String token) {
         String email = getUsernameFromToken(token).getEmail();
         User user = getOrElseThrowUser(email);
+        LOGGER.debug("User gathered successfully: {}", user.getEmail());
         return UserResponse.builder()
                 .id(user.getId())
                 .name(user.getName())
--- src/main/java/com/estate/estateserver/services/RentalServices.java
+++ src/main/java/com/estate/estateserver/services/RentalServices.java
@@ -4,10 +4,13 @@ import com.estate.estateserver.exceptions.RepositoryException;
 import com.estate.estateserver.mappers.IRentalMapper;
 import com.estate.estateserver.models.entities.Rental;
 import com.estate.estateserver.models.requests.RentalListRequest;
+import com.estate.estateserver.models.responses.MessageResponse;
 import com.estate.estateserver.models.responses.RentalListResponse;
 import com.estate.estateserver.models.responses.RentalResponse;
 import com.estate.estateserver.repositories.IRentalRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,11 +18,9 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-
 public class RentalServices {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(RentalServices.class);
     private final IRentalRepository rentalRepository;
-
 
     public RentalListResponse getAllRentals() {
         RentalListResponse getRentalListResponse = new RentalListResponse();
@@ -34,13 +35,13 @@ public class RentalServices {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        LOGGER.debug("Rentals gathered : {}", getRentalListResponse);
         return getRentalListResponse;
     }
 
-    public RentalListResponse postRentals(RentalListRequest rentalListToPost) {
+    public MessageResponse postRentals(RentalListRequest rentalListToPost) {
         //Init response object
-        RentalListResponse postRentalResponse = new RentalListResponse();
+        String message = null;
         //Try catch block to handle exceptions
         try {
             List<Rental> rentalsList = IRentalMapper.INSTANCE.rentalListRequestToRentalResponseList(rentalListToPost.getRentals());
@@ -49,14 +50,17 @@ public class RentalServices {
             //2.Verify if there are rentals and return the list of rentals on a response object
             if (!rentalsList.isEmpty()) {
                 List<RentalResponse> responses = IRentalMapper.INSTANCE.rentalListToRentalResponseList(rentalsList);
-                postRentalResponse = RentalListResponse.builder().rentals(responses).build();
+                message = "Rentals created !";
+                LOGGER.debug("{}: {}", message, responses);
             } else {
+                message = "Rentals not created !";
                 throw new RepositoryException("Rental list not found", rentalListToPost.toString());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return postRentalResponse;
+
+        return MessageResponse.builder().message(message).build();
     }
 
     public RentalResponse getRentalById(int id) {
@@ -74,7 +78,40 @@ public class RentalServices {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        LOGGER.debug("Rental gathered : {}", rentalResponse);
         return rentalResponse;
+    }
+
+    public MessageResponse putRentalPerId(int id, RentalResponse rentalResponse) {
+        String message;
+        try {
+            //1. Retrieve rental by id
+            Rental rentalFromDb = findById(id);
+            Rental rentalToSave = IRentalMapper.INSTANCE.rentalResponseToRental(rentalResponse);
+            //2. Verify if rental exists and is ok to update
+            message = verifyRentalAndReturnResponseMessage(rentalFromDb, rentalToSave);
+        } catch (Exception e) {
+            message = e.getMessage();
+            e.printStackTrace();
+        }
+        LOGGER.debug("Rental updated : {}", message);
+        return MessageResponse.builder().message(message).build();
+    }
+
+    private String verifyRentalAndReturnResponseMessage(Rental rentalFromDb, Rental rentalToSave) {
+        String message;
+        if ((rentalFromDb != null) && rentalToSave.getId() == rentalFromDb.getId()) {
+            //3. Persist in database
+            rentalToSave = saveRental(rentalToSave);
+            if (rentalToSave != null) {
+                message = "Rental updated successfully";
+            } else {
+                message = "Rental not updated";
+            }
+        } else {
+            throw new RepositoryException(String.format("rental with id %s not found", rentalToSave.getId()), rentalToSave.toString());
+        }
+        return message;
     }
 
     @Transactional
@@ -92,4 +129,10 @@ public class RentalServices {
         return rentalRepository.findById(id)
                 .orElse(null);
     }
+
+    @Transactional
+    Rental saveRental(Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
 }
