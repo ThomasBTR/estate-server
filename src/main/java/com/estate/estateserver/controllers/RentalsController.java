1e28fb49abe2f8519b324733213566ef3eae7d20
ThomasBTR <thomatito971@gmail.com>
2023-03-15 15:12:47 +0100

    updated doc

--- src/main/java/com/estate/estateserver/controllers/RentalsController.java
+++ src/main/java/com/estate/estateserver/controllers/RentalsController.java
@@ -10,53 +10,51 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/rentals")
 @RequiredArgsConstructor
-@Tag(name = "Rentals", description = "The Rental API")
 @SecurityRequirement(name = "token")
 public class RentalsController {
 
     private final RentalServices rentalServices;
 
 
-    @Operation(summary = "Find all rentals")
+    @Operation(summary = "Find all rentals", tags = {"Rentals"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Rentals retrieved",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = RentalListResponse.class))}),
             @ApiResponse(responseCode = "401", description = "Access denied",
                     content = @Content)})
-    @GetMapping(value = "/rentals", produces = "application/json")
+    @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity<RentalListResponse> getRentals() {
         return ResponseEntity.ok(rentalServices.getAllRentals());
     }
 
-    @Operation(summary = "save a rental list")
+    @Operation(summary = "Save a rental list", tags = {"Rentals"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Rentals saved and returned",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = RentalListResponse.class))}),
             @ApiResponse(responseCode = "401", description = "Access denied",
                     content = @Content)})
-    @PostMapping(value = "/rentals", consumes = "application/json", produces = "application/json")
+    @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
     public ResponseEntity<RentalListResponse> postRentals(@RequestBody RentalListRequest rentalListRequest) {
         return ResponseEntity.ok(rentalServices.postRentals(rentalListRequest));
     }
 
-    @Operation(summary = "Find rental by id")
+    @Operation(summary = "Get rental by id", tags = {"Rental per id"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Rental retrieved",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = RentalListResponse.class))}),
             @ApiResponse(responseCode = "401", description = "Access denied",
                     content = @Content)})
-    @GetMapping(value = "/rentals/{id}", produces = "application/json")
+    @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<RentalResponse> getRentalById(@PathVariable int id) {
         return ResponseEntity.ok(rentalServices.getRentalById(id));
     }
