667a5531618d6fb053ab805d249e9a6edfd4d20d
ThomasBTR <thomatito971@gmail.com>
2023-03-26 22:08:47 +0200

    start to save locally an image. Saving is ok, but get back the image is not correctly working

---
+++ src/main/java/com/estate/estateserver/configurations/ResourceWebConfig.java
@@ -0,0 +1,22 @@
+package com.estate.estateserver.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceWebConfig implements WebMvcConfigurer {
+    final Environment environment;
+
+    public ResourceWebConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        String location = environment.getProperty("estate.file.storage-mapping", "file:src/main/resources/static/images");
+
+        registry.addResourceHandler("/uploads/**").addResourceLocations(location);
+    }
+}
